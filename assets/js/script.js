b1ffc11f037bfc276f957ef2855c6586d8575f5d
oldgraybuzzard <k_felder@me.com>
2021-09-17 19:41:49 -0400

    drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ $(".list-group").on("click", "p", function() {
   var text = $(this)
   .text()
   .trim();
-  console.log(text);
+ 
 
   var textInput = $("<textarea>")
     .addClass("form-control")
@@ -184,4 +184,62 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+// sortable
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+         },
+    deactivate: function(event) {
+         },
+    over: function(event) {
+         },
+    update: function(event) {
+      var tempArr = [];
+      //loop over current set of children in sortable list
+      $(this).children().each(function() {
+        var text = $(this)
+        .find("p")
+        .text()
+        .trim();
 
+        var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+
+        //add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date
+        });
+      });
+      
+        // trim down list of ID's to match object property
+        var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+        
+        //update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+     }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
